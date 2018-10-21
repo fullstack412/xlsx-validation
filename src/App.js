f121650e4b4d363968c3cfaba3b83284ebeb6dec
fullstack412 <dt897867@gmail.com>
2018-10-21 15:33:45 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import XLSX from 'xlsx';
+import XLSX from 'data-validation-xlsx';
 import saveAs from 'file-saver';
+
 
 class App extends Component {
   constructor(props){
@@ -12,7 +13,7 @@ class App extends Component {
   s2ab(s){
     var buf = new ArrayBuffer(s.length);
     var view = new Uint8Array(buf);
-    for(var i = 0 ; i < s.length ; i++) view[i] = s. charCodeAt(i) & 0xFF;
+    for(var i = 0 ; i < s.length ; i++) view[i] = s.charCodeAt(i) & 0xFF;
     return buf;
   }
   convert = () => {
@@ -25,11 +26,25 @@ class App extends Component {
     };
     wb.SheetNames.push('New Sheet');
     var ws = XLSX.utils.json_to_sheet([
-      { Student: "Euan", Subject: "" },
-      { Student: "Mary", Subject: "" },
-      { Student: "Holly", Subject: "" },
-    ], {header:["Student","Subject"]});
-
+      { Student: "Euan"},
+      { Student: "Mary"},
+      { Student: "Holly"},
+    ], {header:["Student","Subject", "Grade", "Email", "Phone Number", "Date", "Gender"]});
+    ws['!dataValidation'] =  [
+      {sqref: 'A2:A99', type: 'alphabet'},
+      {sqref: 'B2:B99', type: 'list', values: ['Maths', 'English', 'History', 'Geography', 'Art', 'Science', 'Computers', 'French']},
+      {sqref: 'C2:C99', type: 'decimal', operator: 'between', min:1, max: 10},
+      {sqref: 'D2:D99', type: 'email'},
+      {sqref: 'E2:E99', type: 'phone_number'},
+      {sqref: 'F2:F99', type: 'date', operator: 'between', start: '1/1/1900', end: '12/31/3000'},
+      {sqref: 'G2:G99', type: 'list', values: ['Male', 'Female']}
+    ];
+    ws['!cols'] = [{wch:16},{wch:16},{wch:16},{wch:16},{wch:16},{wch:16},{wch:16}]
+    for(let i=0;i<97;i++){
+      ws['F'+(2+i)] = {v:'', z: 'mm/dd/yyyy'};
+      ws['E'+(2+i)] = {v:'', z: '@'};
+    }
+    ws['!ref']="A1:G99";
 
     wb.Sheets['New Sheet'] = ws;
 
